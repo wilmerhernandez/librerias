8bb1f5f9de8c3c5a40f8ea3396fd341a2ed46695
Wilmer R. Hernandez B <37345506+wilmerhernandez@users.noreply.github.com>
2021-04-18 18:30:21 -0500

    montando librerias

---
+++ libreria.cpp
@@ -0,0 +1,19 @@
+char *devolverletra(char c){
+	switch (c){
+		case 'h':
+			return "44";
+		break;
+		case 'H':
+			return "#44";
+		break;
+		case 'o':
+			return "666";
+		break;	
+		case 'l':
+			return "555";
+		break;
+		case 'a':
+			return "2";
+		break;
+	}
+}
